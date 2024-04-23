d9ae85f39cf9aad279256693da36cfa32c3854d8
Tanmaya Panda <tanmayapanda@microsoft.com>
2024-04-23 18:34:24 +0530

    fix invisible button

--- webpack.common.js
+++ webpack.common.js
@@ -28,6 +28,16 @@ module.exports = {
           'css-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
       }
     ]
   },
