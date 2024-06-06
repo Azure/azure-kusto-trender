f89704c5174c014c8eff8b6e1ee16f4b8193f884
ramacg <ramacg@microsoft.com>
2024-06-06 14:29:59 +0530

    * Load assets through asset-resource

--- webpack.common.js
+++ webpack.common.js
@@ -30,15 +30,10 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jp(e*)g|svg)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 8000, // Convert images < 8kb to base64 strings
-            name: 'images/[hash]-[name].[ext]'
-          }
-        }]
-      }
+        
+        test: /\.(jp(e*)g|png|svg|gif)$/,
+        type: 'asset/resource',
+      },
     ]
   },
   resolve: {
